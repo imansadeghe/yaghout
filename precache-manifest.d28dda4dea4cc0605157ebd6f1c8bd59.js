a6665fe0cf97155943c0955064f5f10c5ffecc21
imansadeghe <imansadeghe@gamil.com>
2020-01-30 16:29:08 +0330

    Updates

---
+++ precache-manifest.d28dda4dea4cc0605157ebd6f1c8bd59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307cf27a3909e26e9b2dbe765e6021d9",
+    "url": "/yaghout/index.html"
+  },
+  {
+    "revision": "e536f63f633bef2bca6c",
+    "url": "/yaghout/static/css/main.1144373a.chunk.css"
+  },
+  {
+    "revision": "23ac04bc45a2864547f3",
+    "url": "/yaghout/static/js/2.6219a48b.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/yaghout/static/js/2.6219a48b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e536f63f633bef2bca6c",
+    "url": "/yaghout/static/js/main.b17289d6.chunk.js"
+  },
+  {
+    "revision": "64d951221c918a5a1155",
+    "url": "/yaghout/static/js/runtime-main.14f6398f.js"
+  },
+  {
+    "revision": "e8354ea8e6c1432563271acb2f80cc30",
+    "url": "/yaghout/static/media/danstevis.e8354ea8.otf"
+  }
+]);
